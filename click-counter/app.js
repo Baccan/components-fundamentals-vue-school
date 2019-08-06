38158e998c9c8fc1c209652805895fe2e94b2dc3
Gustavo Baccan Gomes <gustavo.gomes@butantan.gov.br>
2019-08-06 07:58:00 -0300

    Component's Template

--- click-counter/app.js
+++ click-counter/app.js
@@ -1,6 +1,7 @@
 // Vue.component(name, {});
 Vue.component("click-counter", {
-  template: '<button @click="count++">{{count}}</button>',
+  // template: '<button @click="count++">{{count}}</button>',
+  template: "#click-counter-template",
   data: () => ({
     count: 0
   })
