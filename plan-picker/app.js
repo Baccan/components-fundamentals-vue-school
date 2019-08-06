8bc66c42ca7bdbae1a2f740780869663b8326b18
Gustavo Baccan Gomes <gustavo.gomes@butantan.gov.br>
2019-08-06 08:22:23 -0300

    Reusable Components with Props

---
+++ plan-picker/app.js
@@ -0,0 +1,20 @@
+Vue.component("plan", {
+  template: "#plan-template",
+  // props: ["name"] // definir as props como um array não temos muito controle
+  // Uma boa pratica e declara-lo como um objeto
+  props: {
+    name: {
+      type: String,
+      // default: "Baccan",
+      required: true
+    }
+    // price: Number
+  }
+});
+
+new Vue({
+  el: "#app",
+  data: {
+    plans: ["The Single", "The Curious", "The Addict"]
+  }
+});
